95e7e3b01ac8f8b5ed23a8480a849ffabf39b54b
KhaledAwashreh <khaled.m.awashreh@gmail.com>
2026-04-29 12:00:00 +0300

    feat: export utility functions from auth module

--- dalil/auth/__init__.py
+++ dalil/auth/__init__.py
@@ -6,5 +6,9 @@ Provides OAuth2/OpenID Connect integration with Atlassian, OpenAI, and Anthropic
 
 from dalil.auth.models import User, Token, ProviderType, OAuthState
 from dalil.auth.storage import TokenStorage
+from dalil.auth.utils import get_token_for_provider, is_authenticated
 
-__all__ = ["User", "Token", "ProviderType", "OAuthState", "TokenStorage"]
+__all__ = [
+    "User", "Token", "ProviderType", "OAuthState",
+    "TokenStorage", "get_token_for_provider", "is_authenticated",
+]
